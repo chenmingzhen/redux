8cff743ba39a59eaf5e59368a89d6dd3f09771ee
chenmingzhen <214164051@qq.com>
2021-03-09 13:11:18 +0800

    add symbol-observable docs

--- src/utils/symbol-observable.ts
+++ src/utils/symbol-observable.ts
@@ -1,8 +1,19 @@
+// global全局声明  
+// Symbol的observable处于提案中
+// 重写SymbolConstructor的声明
+// 点击进去Symbol的声明见SymbolConstructor的详情
+// 这里对SymbolConstructor的声明进行拓展
 declare global {
   interface SymbolConstructor {
     readonly observable: symbol
   }
 }
+
+// declare namespace Test{
+//   export class Calc{
+//     doSUm(limit:number):void
+//   }
+// }
 
 const $$observable = /* #__PURE__ */ (() =>
   (typeof Symbol === 'function' && Symbol.observable) || '@@observable')()
